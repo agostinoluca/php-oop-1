56daf24cebe451f091b757f755ed5a1a7923c58b
agostinoluca <agostino.luca89@gmail.com>
2024-03-14 20:08:28 +0100

    fix foreach for display cards in page

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 
 class Movie
 {
-    public $title;
-    public $duration;
-    public $poster;
-    public $genre;
+    // public $title;
+    // public $duration;
+    // public $poster;
+    // public $genre;
 
-    public function __construct($title, $duration, $poster, $genre)
+    /**
+     * @param String $title The title of movie
+     * @param Number $duration The duration of movie
+     * @param String $poster The poster of movie
+     * @param String $genre The genre of movie
+     */
+    public function __construct(public string $title, public int $duration, public string $poster, public string $genre)
     {
         $this->title = $title;
         $this->duration = $duration;
@@ -24,14 +30,13 @@ class Movie
     }
 }
 
-$Robocop = new Movie('Robocop', '120', 'https://picsum.photos/id/684/600/400', 'Action');
-$Terminator = new Movie('Terminator', '140', 'https://picsum.photos/id/684/600/400', 'Action');
-$Fantozzi = new Movie('Fantozzi', '80', 'https://picsum.photos/id/684/600/400', 'Comedy');
-$IlPadrino = new Movie('Il Padrino', '110', 'https://picsum.photos/id/684/600/400', 'Gangster');
 
-// var_dump($Robocop, $IlPadrino, $Fantozzi, $IlPadrino);
-
-
+$movies = [
+    new Movie('Robocop', 120, 'https://picsum.photos/id/684/600/400', 'Action'),
+    new Movie('Terminator', 140, 'https://picsum.photos/id/684/600/400', 'Action'),
+    new Movie('Fantozzi', 80, 'https://picsum.photos/id/684/600/400', 'Comedy'),
+    new Movie('Il Padrino', 110, 'https://picsum.photos/id/684/600/400', 'Gangster')
+]
 
 ?>
 
@@ -46,12 +51,26 @@ $IlPadrino = new Movie('Il Padrino', '110', 'https://picsum.photos/id/684/600/40
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
 
-<body>
+<body class="bg-secondary">
     <div class="container">
-        <?php
-        $Robocop->displayInfo();
-        $Terminator->displayInfo();
-        ?>
+        <div class="row">
+            <div class="col-12 text-center text-light mt-3">
+                <h1>Top Movies</h1>
+            </div>
+            <?php foreach ($movies as $movie) : ?>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 g-3">
+                    <div class="card bg-success">
+                        <div class="card-body text-light">
+                            <?php
+                            $movie->displayInfo();
+                            echo "<br><br>";
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+
     </div>
 </body>
 
