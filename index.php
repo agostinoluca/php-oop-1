87cf9242176428bdd9ca6ec3a52ac665bef216a1
agostinoluca <agostino.luca89@gmail.com>
2024-03-16 10:19:18 +0100

    add folder database

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php
-
 require_once __DIR__ . '/app/Models/Movie.php';
-
-$movies = [
-    new Movie('Robocop', 120, 'https://picsum.photos/id/521/600/400', 'Action'),
-    new Movie('Terminator', 140, 'https://picsum.photos/id/684/600/400', 'Action'),
-    new Movie('Fantozzi', 80, 'https://picsum.photos/id/695/600/400', 'Comedy'),
-    new Movie('Il Padrino', 110, 'https://picsum.photos/id/64/600/400', 'Gangster'),
-    new Movie('Forrest Gump', 142, 'https://picsum.photos/id/237/600/400', 'Drama'),
-    new Movie('Pulp Fiction', 154, 'https://picsum.photos/id/674/600/400', 'Crime'),
-    new Movie('Interstellar', 169, 'https://picsum.photos/id/903/600/400', 'Science Fiction'),
-    new Movie('Inception', 148, 'https://picsum.photos/id/870/600/400', 'Thriller'),
-    new Movie("Il Signore degli Anelli: La Compagnia dell'Anello", 178, 'https://picsum.photos/id/63/600/400', 'Fantasy'),
-    new Movie('Matrix', 136, 'https://picsum.photos/id/23/600/400', 'Action'),
-    new Movie('La vita è bella', 116, 'https://picsum.photos/id/494/600/400', 'Comedy'),
-    new Movie("Schindler's List", 195, 'https://picsum.photos/id/139/600/400', 'Biographical'),
-    new Movie('The Dark Knight', 152, 'https://picsum.photos/id/593/600/400', 'Superhero'),
-    new Movie('Fight Club', 139, 'https://picsum.photos/id/1074/600/400', 'Drama')
-
-]
-
+require_once __DIR__ . '/database/db.php';
 ?>
 
 <?php require_once __DIR__ . '/app/Layout/head.php'; ?>
